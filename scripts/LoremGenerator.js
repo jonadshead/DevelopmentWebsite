ff354750c3dfa6ca27cac1cfd56878678ab6813c
Jonathan Adshead <jon.adshead@ntlworld.com>
2019-10-09 16:16:59 +0100

    added lorem ipsum generator.

---
+++ scripts/LoremGenerator.js
@@ -0,0 +1,100 @@
+function GenerateLoremString(wordsAmount)
+{
+    let str = "";
+    let totalAmount = 0;
+    let sentenceLength = GetRandomSentenceLength(wordsAmount);
+
+    while (totalAmount < wordsAmount)
+    {
+        for(let i = 0; i < sentenceLength - 1; i++)
+        {
+            if (IsSeparator())
+                strTemp = GetRandomWord() + ", ";
+            else
+                strTemp = GetRandomWord() + " ";
+
+            
+            if (i == 0)
+                strTemp = strTemp.charAt(0).toUpperCase() + strTemp.substring(1);
+
+            str += strTemp;
+        }
+
+        str += GetRandomWord() + ". ";
+
+        totalAmount += sentenceLength;
+    }
+    
+    return str;
+}
+
+let Words = [
+    'lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur',
+    'adipiscing', 'elit', 'curabitur', 'vel', 'hendrerit', 'libero',
+    'eleifend', 'blandit', 'nunc', 'ornare', 'odio', 'ut',
+    'orci', 'gravida', 'imperdiet', 'nullam', 'purus', 'lacinia',
+    'a', 'pretium', 'quis', 'congue', 'praesent', 'sagittis', 
+    'laoreet', 'auctor', 'mauris', 'non', 'velit', 'eros',
+    'dictum', 'proin', 'accumsan', 'sapien', 'nec', 'massa',
+    'volutpat', 'venenatis', 'sed', 'eu', 'molestie', 'lacus',
+    'quisque', 'porttitor', 'ligula', 'dui', 'mollis', 'tempus',
+    'at', 'magna', 'vestibulum', 'turpis', 'ac', 'diam',
+    'tincidunt', 'id', 'condimentum', 'enim', 'sodales', 'in',
+    'hac', 'habitasse', 'platea', 'dictumst', 'aenean', 'neque',
+    'fusce', 'augue', 'leo', 'eget', 'semper', 'mattis', 
+    'tortor', 'scelerisque', 'nulla', 'interdum', 'tellus', 'malesuada',
+    'rhoncus', 'porta', 'sem', 'aliquet', 'et', 'nam',
+    'suspendisse', 'potenti', 'vivamus', 'luctus', 'fringilla', 'erat',
+    'donec', 'justo', 'vehicula', 'ultricies', 'varius', 'ante',
+    'primis', 'faucibus', 'ultrices', 'posuere', 'cubilia', 'curae',
+    'etiam', 'cursus', 'aliquam', 'quam', 'dapibus', 'nisl',
+    'feugiat', 'egestas', 'class', 'aptent', 'taciti', 'sociosqu',
+    'ad', 'litora', 'torquent', 'per', 'conubia', 'nostra',
+    'inceptos', 'himenaeos', 'phasellus', 'nibh', 'pulvinar', 'vitae',
+    'urna', 'iaculis', 'lobortis', 'nisi', 'viverra', 'arcu',
+    'morbi', 'pellentesque', 'metus', 'commodo', 'ut', 'facilisis',
+    'felis', 'tristique', 'ullamcorper', 'placerat', 'aenean', 'convallis',
+    'sollicitudin', 'integer', 'rutrum', 'duis', 'est', 'etiam',
+    'bibendum', 'donec', 'pharetra', 'vulputate', 'maecenas', 'mi',
+    'fermentum', 'consequat', 'suscipit', 'aliquam', 'habitant', 'senectus',
+    'netus', 'fames', 'quisque', 'euismod', 'curabitur', 'lectus',
+    'elementum', 'tempor', 'risus', 'cras',
+];
+
+function GetRandomWord()
+{
+    return Words[Math.ceil(Math.random() * Words.length) - 1];
+}
+
+function GetRandomSentenceLength(wordsAmount)
+{
+    let f = Math.ceil(Math.random() * 20) + 5;
+
+    if (f > wordsAmount)
+        f = wordsAmount;
+
+    return f;
+}
+
+function IsSeparator()
+{
+    return Math.ceil(Math.random() * 10) == 10;
+}
+
+function CallGenerator()
+{
+    let i = prompt("Amount of Words:");
+    document.getElementById("GeneratorOutput").innerHTML = GenerateLoremString(TryParseInt(i));
+}
+
+function TryParseInt(str)
+{
+    let retValue = 0;
+
+    if(str !== null)
+        if(str.length > 0)
+            if (!isNaN(str))
+                retValue = parseInt(str);
+
+    return retValue;
+}
